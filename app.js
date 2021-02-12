49f91e195242ca3d478aaa41d412134380642726
Shrishtijain991 <shrishti.jain991@gmail.com>
2021-02-12 14:36:00 +0530

    Improved stripe implementation 3

--- app.js
+++ app.js
@@ -63,9 +63,10 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
 app.post(
   '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
+  bodyParser.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
